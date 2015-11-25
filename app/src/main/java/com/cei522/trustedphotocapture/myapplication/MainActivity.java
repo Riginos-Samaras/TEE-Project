bb9e7a31e28aed2b0970a87e867401cc9885aeee
Riginos Samaras <Riginos Samaras>
2015-11-25 22:25:34 +0200

    second

---
+++ app/src/main/java/com/cei522/trustedphotocapture/myapplication/MainActivity.java
@@ -0,0 +1,65 @@
+package com.cei522.trustedphotocapture.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.util.zip.Adler32;
+
+public class MainActivity extends Activity {
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView imageView;
+    private ImageView imageView2;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.imageView = (ImageView)this.findViewById(R.id.imageView1);
+        this.imageView2 = (ImageView)this.findViewById(R.id.imageView2);
+        Button photoButton = (Button) this.findViewById(R.id.button1);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            String paok = BitMapToString((Bitmap) data.getExtras().get("data"));
+            Log.w("INFO 8============D", paok);
+            imageView.setImageBitmap(photo);
+            imageView2.setImageBitmap(StringToBitMap(paok));
+        }
+    }
+    public String BitMapToString(Bitmap bitmap){
+        ByteArrayOutputStream baos=new  ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
+        byte [] b=baos.toByteArray();
+        String temp= Base64.encodeToString(b, Base64.DEFAULT);
+        return temp;
+    }
+    public Bitmap StringToBitMap(String encodedString){
+        try {
+            byte [] encodeByte=Base64.decode(encodedString,Base64.DEFAULT);
+            Bitmap bitmap= BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        } catch(Exception e) {
+            e.getMessage();
+            return null;
+        }
+    }
+}
